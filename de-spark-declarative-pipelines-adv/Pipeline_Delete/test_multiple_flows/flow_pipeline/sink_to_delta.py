a4d66b2205361ec76ae105ab5edbfc2191cca17f
cchalc <cchalc>
2026-01-06 04:54:22 +0000

    reset

--- de-spark-declarative-pipelines-adv/Pipeline_Delete/test_multiple_flows/flow_pipeline/sink_to_delta.py
+++
@@ -1,8 +0,0 @@
-# from pyspark import pipelines as dp
-
-
-# dp.create_sink(
-#   name = "delta_sink",
-#   format = "delta",
-#   options = { "tableName": "multi_flows_2_silver.orders_silver_flows_demo" }
-# )
